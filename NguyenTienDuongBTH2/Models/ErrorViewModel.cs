3c28bdf37ec50fe7541915c737688069becdd2f8
Admin <dn1733630@gmail.com>
2022-11-02 18:14:10 +0700

    NguyenTienDuongBTH2

---
+++ NguyenTienDuongBTH2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenTienDuongBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
